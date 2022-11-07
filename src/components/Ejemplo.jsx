465f8be1022bb08051ed65c83a1c82a92eedc2eb
Lopezzz099 <nacholopez1903@gmail.com>
2022-11-07 17:11:29 -0300

    segundo commit

--- src/components/Ejemplo.jsx
+++ src/components/Ejemplo.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
-const Ejemplo = () => {
+const App = () => {
+    const [message, setMessate] = useState("");
+    const [secondMessage, setSecondMessage] = useState("")
+
+    const handleOne = () => {
+        setMessate("Hola camada 3")
+    }
+
+    const handleSecond = (value) => {
+        setSecondMessage(value);
+    }
+    //readibility
     return (
-        <div>Ignacio Lopez</div>
+        <div>
+            <h1>Clase práctica 9</h1>
+            <p>El mensaje uno: {message}</p>
+            <button onClick={handleOne}>Obtener saludo</button>
+
+            <ul>
+                {
+                    ["Jaider", "Ester", "Pedro", "Pablo"].map(name => (
+                        <li key={name}>
+                            <button onClick={() => handleSecond(name)}>Escoger person para saludar</button>
+                        </li>
+                    ))
+                }
+                <p>Este saludo es para: {secondMessage}</p>
+            </ul>
+        </div>
     )
 }
 
-export default Ejemplo
+export default App
